ad9968093ed9ab853c7f43bec3e9f85a7b902eab
Elias Biscaia <e.biscaia@techbit.com.ar>
2022-04-19 15:51:06 -0300

    Se corrigio excluir sucursal

--- ej4/resumen.sh
+++ ej4/resumen.sh
@@ -5,32 +5,41 @@
 #   -h: ayuda
 #   -e: Sucursal excluida (opcional)
 #   -o: directorio de salida (opcional)
-
-while getopts  d:o:h opt;do
+directorio="./"
+directorio_salida="./"
+sucursal_excluida=""
+while getopts d:e:o:h opt; do
     case $opt in
         d)
             directorio=$OPTARG
             ;;
+        e)
+            sucursal_excluida=$OPTARG
+            ;;
         o)
-            output=$OPTARG
+            directorio_salida=$OPTARG
             ;;
         h)
-            echo "Uso: $0 -d nombre_directorio -o nombre_directorio_salida"
+            echo "Uso: $0 -d directorio -e sucursal_excluida -o directorio_salida"
             exit 0
             ;;
         \?)
-            echo "Uso: $0 -d nombre_directorio -o nombre_directorio_salida"
+            echo "Uso: $0 -d directorio -e sucursal_excluida -o directorio_salida"
             exit 1
             ;;
     esac
 done
 
 #csv to json
-productos=$(find $directorio -name "*.csv" -exec cat {} \; | awk '{print $1}' | sed '1d' | awk -F "," 'BEGIN{OFS=",";} {arr[$1]+=$2} END {for (i in arr) {print i,arr[i]}}')
+productos=$(find $directorio -iname "*.csv" -type f ! -name "$sucursal_excluida.csv" -exec cat {} \;   |
+            awk '{print $1}' |
+            sed '1d' | sort |
+            awk -F "," 'BEGIN{OFS=",";} {arr[$1]+=$2} END {for (i in arr) {print i,arr[i]}}')
+
 productos_json=$(echo $productos | #convertir a json
         sed 's/,/:/g' | #reemplazar comas por dos puntos
         sed 's/ /, /g' | #remplazar espacios por comas
         sed 's/^/{/g' | #agregar corchetes
         sed 's/$/}/g') #agregar corchetes
 
-echo $productos_json > $output/productos.json
+echo $productos_json > ./$directorio_salida/salida.json
