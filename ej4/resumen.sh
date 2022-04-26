b64abb07fe777e86ce60e1dc7a0991832d82512c
Elias Biscaia <e.biscaia@techbit.com.ar>
2022-04-26 14:26:17 -0300

    se actualiza el ejercio 4

--- ej4/resumen.sh
+++ ej4/resumen.sh
@@ -11,6 +11,7 @@
 #--porc [NÚMERO ENTERO]: si la similitud entre archivos es mayor o igual al número pasado
 #se informan, sino se desestiman.
 #--salida ARCHIVO: Se solicita la salida a un archivo, caso contrario se muestra por pantalla
+#--dir ARCHIVO: Se solicita la salida a un archivo, caso contrario se muestra por pantalla
 #como cualquier comando. Debe estar formateada para ser legible y estética.
 #--help: muestra la ayuda del script.
 
@@ -29,18 +30,18 @@ usage(){
     echo "--porc [NÚMERO ENTERO]: si la similitud entre archivos es mayor o igual al número pasado"
     echo "se informan, sino se desestiman."
     echo "--salida ARCHIVO: Archivo de salida"
-    echo "como cualquier comando. Debe estar formateada para ser legible y estética."
+    echo "--dir ARCHIVO: Directorio de salida"
     echo "--help: muestra la ayuda del script."
     echo "--porc [NÚMERO ENTERO]: si la similitud entre archivos es mayor o igual al número pasado"
-    echo "Ejemplo: $0 --ext=extensiones.txt --porc=50 --salida=salida.txt"
+    echo "Ejemplo: $0  --dir=test --ext=extensiones.txt --porc=50 --salida=salida.txt"
+    echo "Ejemplo: $0  --dir test --ext ./extensiones/extensiones.txt --porc=50 --salida=salida.txt"
     exit 1;
 }
-comentarios=1
-directorio="."
-salida="./salida.txt"
+coment=0
+sincom=0
 HELP=0
 # #Seteo los parametros
-PARSED_ARGUMENTS=$(getopt -a -o hp: --long help:,porc:,coment,sincom,ext:,salida:: -- "$@")
+PARSED_ARGUMENTS=$(getopt -a -o hp: --long help,dir:,porc:,coment,sincom,ext:,salida:: -- "$@")
 result=$?
 if [ "$result" != "0" ]; then
  	usage
@@ -51,10 +52,11 @@ do
  	case "$1" in
  		-h | --help) HELP=1 ; shift ;;
  		--ext ) extensiones=$2 ; shift 2;;
- 		--coment) comentarios=1 ; shift ;;
- 		--sincom ) comentarios=0   ; shift 2 ;;
+ 		--coment) coment=1 ; shift ;;
+ 		--sincom ) sincom=1 ; shift  ;;
  		-p | --porc) porc=$2; shift 2 ;;
         --salida) salida=$2 shift 2 ;;
+        --dir) directorio=$2 shift 2 ;;
         --) shift; break ;;
  		*) echo "Unexpected option: $1 - this should not happen."
           			usage;;
@@ -62,18 +64,53 @@ do
  	esac
 done
 
+validaciones (){
 if [ $HELP -eq 1 ]
 then
        usage        
 fi
 
+if [ $coment -eq 0 ] && [ $sincom -eq 0 ]
+then
+    echo "debe tener solo una de las dos siguientes opciones --coment o --sincom"
+    usage
+fi
 
-echo "Extensiones: $extensiones"
+if [ $coment -eq 1 ] && [ $sincom -eq 1 ]
+then
+    echo "No puede haber parámetros excluyentes --coment y --sincom"
+    usage
+fi
+
+if [ -z $extensiones ]
+then
+    echo "No se pasaron extensiones"
+    usage
+fi
+
+if [ -z $directorio ]
+then
+    echo "No se paso directorio"
+    usage
+fi
+
+if [ -z $porc ]
+then
+    echo "No se paso el porcentaje"
+    usage
+fi
+}
 
 comprobar_diferencias_entre_archivos(){
             cant_linea_archivo1=$(cat $1  | wc -l)
             cant_diff_a1toa2=$(diff  -d  $1 $2  | grep ">" | wc -l)
             cant_diff_a2toa1=$(diff  -d  $2 $1  | grep "<" | wc -l)
+            if [ $coment -eq 0 ] 
+            then
+                cant_linea_archivo1=$(cat $1 | grep  -v "#" | grep  -v "^//" | wc -l)
+                cant_diff_a1toa2=$(diff  -d  $1 $2 | grep ">" | grep  -v "^#" | grep  -v "^//" | wc -l)
+                cant_diff_a2toa1=$(diff  -d  $1 $2 | grep "<" | grep  -v "^#" | grep  -v "^//" | wc -l)
+            fi
             if [ $cant_diff_a1toa2 -gt $cant_diff_a2toa1 ]
             then
                 cant_diff=$cant_diff_a1toa2
@@ -85,15 +122,21 @@ comprobar_diferencias_entre_archivos(){
             result=$(echo "$porcentaje > $porc" | bc -l)
             if [ $result -eq 1 ]
             then
-                echo "Archivo $1 y $2 tienen similitud de $porcentaje%"
-                echo "Archivo $1 y $2 tienen similitud de $porcentaje%" >> $salida
+                
+                if [ -z $salida ]
+                then
+                    echo "Archivo $1 y $2 tienen similitud de $porcentaje%"
+                else
+                    echo "Archivo $1 y $2 tienen similitud de $porcentaje%" >> $salida
+                fi
+                
             fi
 }
 
 # comprobar diferencias entre archivos de la misma extencion
 # comprobar_diferencia [extencion]
 comprobar_diferencias(){
-archivos=$(find ./ -iname "*.$1" -type f -print)
+archivos=$(find $directorio -iname "*.$1" -type f -print)
 for arch1 in $archivos
 do
     for arch2 in $archivos
@@ -110,18 +153,8 @@ do
 done
 }
 
-if [ -z $extensiones ]
-then
-    echo "No se pasaron extensiones"
-    usage
-fi
 
-if [ -z $porc ]
-then
-    echo "No se paso el porcentaje"
-    usage
-fi
-
+validaciones
 extensiones=$(cat $extensiones| tr ";" "\n") #separo las extensiones
 for ext in $extensiones
 do
